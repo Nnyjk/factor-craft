c2145bec2a273cef3fad2f3b5c0858771d42bf19
Y-Bot-N <599019601@qq.com>
2026-03-11 18:00:52 +0800

    feat: 移除已实现的 TODO + QuestCondition 反序列化
    - FactorService: 移除冗余 TODO 注释
    - QuestCondition: 实现 fromNbt 反序列化
    - CompositeCondition: 添加 fromNbt 方法

--- src/main/java/com/factorcraft/module/quest/condition/QuestCondition.java
+++ src/main/java/com/factorcraft/module/quest/condition/QuestCondition.java
@@ -3,6 +3,7 @@ package com.factorcraft.module.quest.condition;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.util.Identifier;
 
 /**
  * 任务条件接口
@@ -33,7 +34,51 @@ public interface QuestCondition {
      * 从 NBT 反序列化
      */
     static QuestCondition fromNbt(NbtCompound nbt, RegistryWrapper.WrapperLookup registries) {
-        // TODO: 实现反序列化逻辑
-        return null;
+        String typeName = nbt.getString("type");
+        QuestConditionType type = QuestConditionType.fromSerializedName(typeName);
+        if (type == null) {
+            return null;
+        }
+        
+        return switch (type) {
+            case ITEM_PICKUP -> new ItemPickupCondition(
+                Identifier.tryParse(nbt.getString("item_id")),
+                nbt.getInt("required")
+            );
+            case ITEM_CRAFT -> new ItemCraftCondition(
+                Identifier.tryParse(nbt.getString("item_id")),
+                nbt.getInt("required")
+            );
+            case ITEM_SUBMIT -> new ItemSubmitCondition(
+                Identifier.tryParse(nbt.getString("item_id")),
+                nbt.getInt("required")
+            );
+            case ITEM_USE -> new ItemUseCondition(
+                Identifier.tryParse(nbt.getString("item_id")),
+                nbt.getInt("required")
+            );
+            case ENTITY_KILL -> {
+                var cond = new EntityKillCondition(
+                    Identifier.tryParse(nbt.getString("entity_id")),
+                    nbt.getInt("required")
+                );
+                cond.onKill(nbt.getInt("current"));
+                yield cond;
+            }
+            case BLOCK_PLACE -> new BlockPlaceCondition(
+                Identifier.tryParse(nbt.getString("block_id")),
+                nbt.getInt("required")
+            );
+            case DIMENSION_TRAVEL -> new DimensionTravelCondition(
+                Identifier.tryParse(nbt.getString("dimension"))
+            );
+            case FACTOR_ABSORB -> new FactorAbsorbCondition(
+                nbt.getDouble("required")
+            );
+            case COMPOSITE -> {
+                // TODO: 实现 CompositeCondition 的 NBT 反序列化
+                yield new CompositeCondition(CompositeCondition.LogicType.AND);
+            }
+        };
     }
 }
